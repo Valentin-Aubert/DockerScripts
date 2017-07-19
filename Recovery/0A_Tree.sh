de9e681d9cf80906faf55a6541a7849badca4abe
semlin <valentin.aubert.45@gmail.com>
2017-07-19 14:51:26 +0200

    Create 0A_Tree.sh

---
+++ Recovery/0A_Tree.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p /home/docker/ghost
+mkdir -p /home/docker/isso
+mkdir -p /home/docker/nginx
+mkdir -p /home/docker/piwik
+mkdir -p /home/docker/scripts/ghostScripts
+mkdir -p /home/docker/scripts/nginxScripts
+mkdir -p /home/docker/scripts/piwikScripts
+mkdir -p /home/docker/scripts/sauvegardes
+mkdir -p /home/docker/archives
